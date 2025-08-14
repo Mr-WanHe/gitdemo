10cbd1f0305f22d8f12f100cac8fcc1119126e37
Mr-WanHe <l2890652966@gmail.com>
2025-08-14 10:56:24 +0800

    add main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: Mr-WanHe l2890652966@gmail.com
+ * @Date: 2025-08-14 10:49:08
+ * @LastEditors: Mr-WanHe l2890652966@gmail.com
+ * @LastEditTime: 2025-08-14 10:49:58
+ * @FilePath: \gitdemo\main.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
